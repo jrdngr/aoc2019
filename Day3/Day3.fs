c3f50387c62e1db858d9f59788cb130c7496ac5f
Jordan Grace <jordan@righteye.com>
2019-12-03 18:21:00 -0500

    Working on laying down paths

--- Day3/Day3.fs
+++ Day3/Day3.fs
@@ -16,7 +16,6 @@ let parseSegment (segment:string) =
     | 'L' -> Left distance
     | _   -> failwith (sprintf "Invalid direction: %c" direction)
 
-
 let input = 
     System.IO.File.ReadLines("Day3/input") 
     |> Seq.map (fun line -> line.Split([|','|]) |> Array.toSeq)
@@ -25,8 +24,47 @@ let input =
 let wire1 = input.[0] |> Seq.map parseSegment
 let wire2 = input.[1] |> Seq.map parseSegment
 
+let manhattanDistance (x1,y1) (x2,y2) =
+    abs(y2 - y1) + abs(x2 - x1)
 
+let distanceFromCenter (x,y) = manhattanDistance (x,y) (0,0)
 
+type Wire = 
+    | Wire1
+    | Wire2
 
+type Cell =
+    | Wire1Present
+    | Wire2Present
+    | CrossedWires
+
+let wireToCell wire =
+    match wire with 
+    | Wire1 -> Wire1Present
+    | Wire2 -> Wire2Present
+
+let mutable grid: Map<int*int,Cell> = Map.empty
+
+let processCell wire position =
+    if Map.containsKey position grid then
+        grid <- match Map.find position grid with
+                | Wire1Present when wire = Wire2 -> grid.Add(position, CrossedWires)
+                | Wire2Present when wire = Wire1 -> grid.Add(position, CrossedWires)
+                | _ -> grid
+    else 
+        grid <- grid.Add(position, wireToCell wire)
+
+let targetPosition (startX, startY) segment=
+   match segment with
+   | Up dist    -> (startX, startY + dist)
+   | Down dist  -> (startX, startY - dist)
+   | Right dist -> (startX + dist, startY)
+   | Left dist  -> (startX - dist, startY)
+
+let addSegment wire (startX, startY) segment =
+   let endX, endY = targetPosition (startX, startY) segment
+   Seq.zip [for x in startX..endX do yield x] [for y in startY..endY do yield y]
+   |> Seq.iter (processCell wire)
+   (endX, endY)
 
 let Run = sprintf "%A" wire2
