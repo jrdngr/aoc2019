f5a86dc9626311eb4855613cf7ad1644e5c23112
Jordan Grace <jordan@righteye.com>
2019-12-03 12:47:57 -0500

    Wrote day 3 input parser

--- Day3/Day3.fs
+++ Day3/Day3.fs
@@ -1,11 +1,32 @@
 module Day3
 
+type PathSegment =
+    | Up of int
+    | Down of int
+    | Right of int
+    | Left of int
+
+let parseSegment (segment:string) =
+    let direction = segment.[0]
+    let distance = segment.[1..] |> int
+    match direction with
+    | 'U' -> Up distance
+    | 'D' -> Down distance
+    | 'R' -> Right distance
+    | 'L' -> Left distance
+    | _   -> failwith (sprintf "Invalid direction: %c" direction)
+
+
 let input = 
     System.IO.File.ReadLines("Day3/input") 
-    |> Seq.map (fun line -> line.Split([|','|]))
+    |> Seq.map (fun line -> line.Split([|','|]) |> Array.toSeq)
     |> Seq.toList
 
-let wire1 = input.[0]
-let wire2 = input.[1]
+let wire1 = input.[0] |> Seq.map parseSegment
+let wire2 = input.[1] |> Seq.map parseSegment
+
+
+
+
 
 let Run = sprintf "%A" wire2
