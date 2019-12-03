c1f5b79c8e7b6c9dfdd5fc61a03fc6e955015ad2
Jordan Grace <jordan@righteye.com>
2019-12-03 10:07:41 -0500

    Set up day 3 input

--- Day3/Day3.fs
+++ Day3/Day3.fs
@@ -1,3 +1,11 @@
 module Day3
 
-let Run = "Day3"
+let input = 
+    System.IO.File.ReadLines("Day3/input") 
+    |> Seq.map (fun line -> line.Split([|','|]))
+    |> Seq.toList
+
+let wire1 = input.[0]
+let wire2 = input.[1]
+
+let Run = sprintf "%A" wire2
