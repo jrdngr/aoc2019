092d9cac3016056f2b034927de88db224bddbcfd
Jordan Grace <jordan@righteye.com>
2019-12-05 18:34:06 -0500

    Finished day 4

--- aoc2019rs/src/solutions/day4.rs
+++ aoc2019rs/src/solutions/day4.rs
@@ -1,14 +1,16 @@
 use anyhow::Result;
 
+use std::collections::HashMap;
+
 // Part 1: 511
-// Part 2: 
+// Part 2: 316
 
 pub fn run() -> Result<String> {
     let range: std::ops::RangeInclusive<usize> = 359282..=820401;
 
     let result = range
         .filter(digits_increasing)
-        .filter(contains_double)
+        .filter(contains_strict_double)
         .count();
 
     Ok(format!("{}", result))
@@ -26,16 +28,29 @@ fn digits_increasing(value: &usize) -> bool {
     true
 }
 
-fn contains_double(value: &usize) -> bool {
+// fn contains_double(value: &usize) -> bool {
+//     let digits = into_digits(value);
+
+//     for i in 0..digits.len()-1 {
+//         if digits[i] == digits[i+1] {
+//             return true;
+//         }
+//     }
+
+//     false
+// }
+
+fn contains_strict_double(value: &usize) -> bool {
     let digits = into_digits(value);
 
-    for i in 0..digits.len()-1 {
-        if digits[i] == digits[i+1] {
-            return true;
-        }
-    }
+    let mut digit_counts = HashMap::new();
 
-    false
+    for i in 0..digits.len() {
+        let count = digit_counts.entry(digits[i]).or_insert(0);
+        *count += 1;
+    }
+    
+    digit_counts.values().any(|&ct| ct == 2)
 }
 
 fn into_digits(value: &usize) -> Vec<usize> {
