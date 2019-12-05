8c3d16728a00248def1ee129d91517a9991037a9
Jordan Grace <jordan@righteye.com>
2019-12-05 15:32:35 -0500

    Added stubs for days 1-5

--- aoc2019rs/src/main.rs
+++ aoc2019rs/src/main.rs
@@ -9,14 +9,18 @@ mod day5;
 fn main() -> Result<()> {
     let day_num = std::env::args().nth(1).ok_or(anyhow!("Please provide a day number as the first argument"))?;
     
-    match day_num.as_ref() {
-        "1" => (),
-        "2" => (),
-        "3" => (),
-        "4" => (),
-        "5" => (),
+    println!("Running day #{}...", day_num);
+
+    let result = match day_num.as_ref() {
+        "1" => day1::run(),
+        "2" => day2::run(),
+        "3" => day3::run(),
+        "4" => day4::run(),
+        "5" => day5::run(),
         _   => bail!("Invalid day number: {}", day_num),
-    }
+    };
+
+    println!("{}", result);
 
     Ok(())
 }
