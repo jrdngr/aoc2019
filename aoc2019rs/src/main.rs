3713a61019509b00e76394581a6d14d4266725bd
Jordan Grace <jordan@righteye.com>
2019-12-05 15:18:56 -0500

    Set up runner

--- aoc2019rs/src/main.rs
+++ aoc2019rs/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::{anyhow, bail, Result};
+
+mod day1;
+mod day2;
+mod day3;
+mod day4;
+mod day5;
+
+fn main() -> Result<()> {
+    let day_num = std::env::args().nth(1).ok_or(anyhow!("Please provide a day number as the first argument"))?;
+    
+    match day_num.as_ref() {
+        "1" => (),
+        "2" => (),
+        "3" => (),
+        "4" => (),
+        "5" => (),
+        _   => bail!("Invalid day number: {}", day_num),
+    }
+
+    Ok(())
 }
