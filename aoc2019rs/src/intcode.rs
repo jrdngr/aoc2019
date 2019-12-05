53ac40172a3a4b67f7d29f9a206ec60bb2812350
Jordan Grace <jordan@righteye.com>
2019-12-05 17:40:30 -0500

    Finished 2-1

--- aoc2019rs/src/intcode.rs
+++ aoc2019rs/src/intcode.rs
@@ -1,8 +1,4 @@
-pub enum IntcodeInstruction {
-    Add(usize, usize, usize),
-    Mul(usize, usize, usize),
-    Halt,
-}
+use anyhow::{bail, Result};
 
 pub struct IntcodeMachine {
     instruction_pointer: usize,
@@ -16,4 +12,53 @@ impl IntcodeMachine {
             memory: machine_code.to_vec(),
         }
     }
+
+    pub fn run(&mut self) -> Result<()> {
+        while let Ok(instruction) = self.read_instruction() {
+            use IntcodeInstruction::*;
+            
+            match instruction {
+                Add(x, y, position) => self.memory[position] = x + y,
+                Mul(x, y, position) => self.memory[position] = x * y,
+                Halt => break,
+            }
+            
+            self.instruction_pointer += 4;
+        }
+
+        Ok(())
+    }
+
+    pub fn read_memory(&self, position: usize) -> usize {
+        self.memory[position]
+    }
+
+    pub fn write_memory(&mut self, position: usize, value: usize) {
+        self.memory[position] = value;
+    }
+
+    fn read_instruction(&self) -> Result<IntcodeInstruction> {
+        if self.instruction_pointer >= self.memory.len() {
+            bail!("Instruction pointer out of range")
+        } else {
+            use IntcodeInstruction::*;
+
+            let mem = &self.memory;
+            let ptr = self.instruction_pointer;
+            
+            Ok(match mem[ptr] {
+                1 => Add(mem[mem[ptr + 1]], mem[mem[ptr + 2]], mem[ptr + 3]),
+                2 => Mul(mem[mem[ptr + 1]], mem[mem[ptr + 2]], mem[ptr + 3]),
+                99 => Halt,
+                _ => bail!("Invalid instruction: {}", mem[ptr]),
+            })
+        }
+        
+    }
+}
+
+pub enum IntcodeInstruction {
+    Add(usize, usize, usize),
+    Mul(usize, usize, usize),
+    Halt,
 }
--- aoc2019rs/src/solutions/day2.rs
+++ aoc2019rs/src/solutions/day2.rs
@@ -1,9 +1,24 @@
 use anyhow::Result;
 
 use crate::utils;
+use crate::intcode::IntcodeMachine;
 
 pub fn run() -> Result<String> {
-    let input = utils::read_input_list_as::<usize>(2, b',')?;
+    let program = utils::read_input_list_as::<usize>(2, b',')?;
+    
+    let part_1_result = run_test(&program, 12, 2)?;
 
-    Ok(String::from(""))
+    Ok(format!("{}", part_1_result))
 }
+
+fn run_test(program: &[usize], input1: usize, input2: usize) -> Result<usize> {
+    let mut machine = IntcodeMachine::new(&program);
+    machine.write_memory(1, input1);
+    machine.write_memory(2, input2);
+    machine.run()?;
+
+    Ok(machine.read_memory(0))
+}
+
+// Part 1: 7594646
+// Part 2: 3376
