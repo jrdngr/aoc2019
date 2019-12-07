bd545d2c23b28a3fa229b67c5486f01c388fd33e
Jordan Grace <jordan.t.grace@gmail.com>
2019-12-07 00:51:04 -0500

    Wrote tests for day 6 parser

--- aoc2019rs/src/solutions/day6.rs
+++ aoc2019rs/src/solutions/day6.rs
@@ -1,5 +1,26 @@
 use anyhow::Result;
+use nom::{
+    IResult,
+    character::complete::alpha1,
+    bytes::complete::tag,
+    sequence::tuple,
+};
 
 pub fn run() -> Result<String> {
     Ok(String::from("Day 6"))
+}
+
+fn parse_orbit(orbit_string: &str) -> IResult<&str, (&str, &str, &str)> {
+    tuple((alpha1, tag(")"), alpha1))(orbit_string)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parser() {
+        assert_eq!(parse_orbit("COM)B").unwrap().1, ("COM", ")", "B"));
+        assert_eq!(parse_orbit("B)C").unwrap().1, ("B", ")", "C"));
+    }
 }
