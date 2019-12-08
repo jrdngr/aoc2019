6d6456e56588e04e798210c4516966fbb44f7a09
Jordan Grace <jordan.t.grace@gmail.com>
2019-12-07 21:48:41 -0500

    Working on graph tests

--- aoc2019rs/src/utils/graph.rs
+++ aoc2019rs/src/utils/graph.rs
@@ -19,17 +19,28 @@ where T: Hash + Eq,
 impl <'a, T> Graph<'a, T>
 where T: Hash + Eq, 
 {
+    /*
+     *
+     * <-- Creatation -->
+     *
+     */
+
     pub fn new() -> Self {
         Default::default()
     }
 
-    pub fn add_node(&mut self, value: T) -> Result<()> {
+    pub fn add_node(&mut self, value: T) {
         if self.adjacency_map.contains_key(&value) {
             self.add_node_with_adjacency_list(value, Vec::new());
-            Ok(())
         } else {
-            bail!("Node is already present")
+            panic!("Node is already present")
         }
+    }
+
+    pub fn add_nodes<I>(&mut self, nodes: I) 
+    where I: IntoIterator<Item = T>
+    {
+        nodes.into_iter().for_each(|node| self.add_node(node))
     }
 
     pub fn add_edge(&mut self, from: &T, to: &'a T) {
@@ -39,15 +50,105 @@ where T: Hash + Eq,
             .push(to);
     }
 
-    pub fn set_adjacency_list(&mut self, node: &T, list: Vec<&'a T>) {
-        // match self.adjacency_map.get_mut(node) {
-        //     Some(list) => 
-        // }
-        // self.adjacency_map.insert(node, list);
+    pub fn add_edges<I>(&mut self, edges: I) 
+    where I: IntoIterator<Item = (&'a T, &'a T)>
+    {
+        edges.into_iter().for_each(|(from, to)| self.add_edge(from, to))
     }
 
     pub fn add_node_with_adjacency_list(&mut self, node: T, list: Vec<&'a T>) {
         self.adjacency_map.insert(node, list);
     }
 
+    /*
+     *
+     * <-- Algorithms -->
+     *
+     */
+
+    pub fn bfs(&self, start: &T, target: &'a T) -> Option<&'a T> {
+        use std::collections::VecDeque;
+        use std::collections::HashSet;
+
+        let mut queue = VecDeque::new();
+        let mut visited = HashSet::new();
+
+        queue.push_back(start);
+        while let Some(next) = queue.pop_front() {
+            if next == target {
+                return Some(target)
+            }
+
+            visited.insert(next);
+            self.adjacency_map
+                .get(next)
+                .expect("Node not found")
+                .into_iter()
+                .for_each(|item| queue.push_back(item));
+        }
+
+        None
+    }
+
+    pub fn dfs(&self, start: &T, target: &'a T) -> Option<&'a T> {
+        use std::collections::HashSet;
+
+        let mut stack = Vec::new();
+        let mut visited = HashSet::new();
+
+        stack.push(start);
+        while let Some(next) = stack.pop() {
+            if next == target {
+                return Some(target)
+            }
+
+            visited.insert(next);
+            self.adjacency_map
+                .get(next)
+                .expect("Node not found")
+                .into_iter()
+                .for_each(|item| stack.push(item));
+        }
+
+        None
+    }
+
+    pub fn path_bfs(&self, start: &'a T, target: &'a T) -> Option<(&'a T, Vec<&'a T>)> {
+        use std::collections::HashSet;
+
+        let mut stack = Vec::new();
+        let mut visited = HashSet::new();
+
+        stack.push((start, Vec::new()));
+        while let Some((next, path)) = stack.pop() {
+            if next == target {
+                return Some((target, path))
+            }
+
+            visited.insert(next);
+            self.adjacency_map
+                .get(next)
+                .expect("Node not found")
+                .into_iter()
+                .for_each(|item| {
+                    let mut item_path = path.clone();
+                    item_path.push(next);
+                    stack.push((item, item_path));
+                });
+        }
+
+        None
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_path() {
+        let mut graph = Graph::new();
+        graph.add_nodes(&[1, 2, 3, 4, 5, 6, 7, 8]);
+
+    }
 }
