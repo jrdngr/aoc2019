e592871b628e20fb677fed2f55a1a0c458805ff3
Jordan Grace <jordan@righteye.com>
2019-12-10 10:55:02 -0500

    Set up input for days 7 and 9

--- aoc2019rs/src/solutions/day7.rs
+++ aoc2019rs/src/solutions/day7.rs
@@ -1,5 +1,9 @@
 use anyhow::{bail, Result};
 
+use crate::utils::input;
+
 pub fn run() -> Result<String> {
+    let _ = input::read_input_list_as::<i64>(7, b',')?;
+
     bail!("Not implemented")
 }
