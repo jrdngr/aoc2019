7637ba64263a4c3a0a6783ee6cf8b0ed49145059
Jordan Grace <jordan@righteye.com>
2019-12-10 14:40:01 -0500

    Set up 7-2

--- aoc2019rs/src/solutions/day7.rs
+++ aoc2019rs/src/solutions/day7.rs
@@ -10,17 +10,39 @@ use crate::utils::intcode::IntcodeMachine;
 
 pub fn run() -> Result<String> {
     let program = input::read_input_list_as::<i64>(7, b',')?;
-    let phase_permutations = math::permutations_cloned::<i64>(&[0, 1, 2, 3, 4]);
 
-    let day1 = phase_permutations.into_iter()
-        .map(|permutation| run_phase_permutation(&program, &permutation))
-        .max()
-        .unwrap();
-
-    Ok(format!("Part 1: {}\nPart 2: {}", day1, ""))
+    Ok(format!("Part 1: {}\nPart 2: {}", run_day_1(&program), run_day_2(&program)))
 }
 
-fn run_phase_permutation(program: &[i64], phases: &[i64]) -> i64 {
+fn run_day_1(program: &[i64]) -> i64 {
+    let phase_permutations = math::permutations_cloned::<i64>(&[0, 1, 2, 3, 4]);
+
+    phase_permutations.into_iter()
+        .map(|permutation| run_day_1_phase_permutation(&program, &permutation))
+        .max()
+        .unwrap()
+}
+
+fn run_day_1_phase_permutation(program: &[i64], phases: &[i64]) -> i64 {
+    let mut next_input = 0;
+    for phase in phases {
+        let output = IntcodeMachine::process_input_single_output(&program.clone(), &[*phase, next_input]).unwrap();
+        next_input = i64::from_str(&output).unwrap();
+    }
+
+    next_input
+}
+
+fn run_day_2(program: &[i64]) -> i64 {
+    let phase_permutations = math::permutations_cloned::<i64>(&[5, 6, 7, 8, 9]);
+    
+    phase_permutations.into_iter()
+        .map(|permutation| run_day_2_phase_permutation(&program, &permutation))
+        .max()
+        .unwrap()
+}
+
+fn run_day_2_phase_permutation(program: &[i64], phases: &[i64]) -> i64 {
     let mut next_input = 0;
     for phase in phases {
         let output = IntcodeMachine::process_input_single_output(&program.clone(), &[*phase, next_input]).unwrap();
