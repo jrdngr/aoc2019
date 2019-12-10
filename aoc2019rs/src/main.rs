9049ff54466aede3d7efedb105bc9ab77122f351
Jordan Grace <jordan@righteye.com>
2019-12-10 10:53:09 -0500

    Set up through day 10

--- aoc2019rs/src/main.rs
+++ aoc2019rs/src/main.rs
@@ -9,13 +9,17 @@ fn main() -> Result<()> {
     println!("Running day #{}...", day_num);
 
     let result = match day_num.as_ref() {
-        "1" => solutions::day1::run()?,
-        "2" => solutions::day2::run()?,
-        "3" => solutions::day3::run()?,
-        "4" => solutions::day4::run()?,
-        "5" => solutions::day5::run()?,
-        "6" => solutions::day6::run()?,
-        _   => bail!("Invalid day number: {}", day_num),
+        "1"  => solutions::day1::run()?,
+        "2"  => solutions::day2::run()?,
+        "3"  => solutions::day3::run()?,
+        "4"  => solutions::day4::run()?,
+        "5"  => solutions::day5::run()?,
+        "6"  => solutions::day6::run()?,
+        "7"  => solutions::day7::run()?,
+        "8"  => solutions::day8::run()?,
+        "9"  => solutions::day9::run()?,
+        "10" => solutions::day10::run()?,
+        _    => bail!("Invalid day number: {}", day_num),
     };
 
     println!("{}", result);
---
+++ aoc2019rs/src/solutions/day7.rs
@@ -0,0 +1,5 @@
+use anyhow::{bail, Result};
+
+pub fn run() -> Result<String> {
+    bail!("Not implemented")
+}
